62c49ac81472383db1e63196039b247283f21b8f
Aravind <aharikrishna@denovosystem.com>
2025-02-17 08:32:05 +0530

    material theme added

--- app/src/main/java/com/HarSu/giftlog/MainActivity.kt
+++ app/src/main/java/com/HarSu/giftlog/MainActivity.kt
@@ -3,27 +3,26 @@ package com.HarSu.giftlog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.HarSu.giftlog.ui.theme.GiftLogTheme
+import com.HarSu.giftlog.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+ // Need to analysis       /* enableEdgeToEdge()*/
         setContent {
-            GiftLogTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Android")
                 }
             }
         }
@@ -34,14 +33,13 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GiftLogTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
